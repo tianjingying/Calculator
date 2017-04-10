dd85a2a1c6371021b63e78fd91a24b88fc78adaf
tianjingying <353555142@qq.com>
2017-04-11 00:31:30 +0800

    解析表达式

--- cal.py
+++ cal.py
@@ -1,8 +1,8 @@
 def init_action(expression):
     """
     # 初始化表达式
-    :param expression: "10+20"
-    :return: res   ['10', '+', '20']
+    :param expression: "-1-20*-3"
+    :return: res   ['-1', '-', '20', '*', '-3']
     """
     res = []
     tag = False
@@ -14,30 +14,37 @@ def init_action(expression):
         else:
         # 不是数字，就是符号
             if tag:
+                #遇到符号，且之前有数字
                 res.append(char)
                 char = ""
                 res.append(i)
                 tag = False
+            else:
+                #遇到符号，之前没数字
+                if i == "-":
+                    char = i
+                else:
+                    res.append(i)
+                pass
     if len(char) > 0:
         res.append(char)
-    print("res: %s"%res)
     return res
 
 def delete_space(expression):
     res = ""
     for i in expression:
-        print("i: %s , len: %s"%(i , len(i)))
         if i == " ":
             continue
         res += i
     pass
-    print("return: %s"%res)
     return res
 
 if __name__ == "__main__":
     print("main")
     # expression = "1 - 2 * ( (60-30 +(-40/5) * (9-2*5/3 + 7 /3*99/4*2998 +10 * 568/14 )) - (-4*3)/ (16-3*2) )"
-    expression = "10 + 20"
+    expression = "-1 - 20*-(3+4)"
     expression = delete_space(expression)
+    print("expression : %s"%expression)
     exp_list = init_action(expression)
+    print("exp_list : %s"%exp_list)
 
